75b3b18bc9dd1609b7fb8598d686c8b42585e5c4
Azimdjan <azimdjan18@gmail.com>
2022-10-05 17:41:29 +0500

    ru locale was done

--- lib/generated/intl/messages_ru.dart
+++ lib/generated/intl/messages_ru.dart
@@ -21,5 +21,37 @@ class MessageLookup extends MessageLookupByLibrary {
   String get localeName => 'ru';
 
   final messages = _notInlinedMessages(_notInlinedMessages);
-  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{};
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "advices": MessageLookupByLibrary.simpleMessage("Советы"),
+        "calendar": MessageLookupByLibrary.simpleMessage("Календарь"),
+        "confirmPassword":
+            MessageLookupByLibrary.simpleMessage("Подтвердить пароль"),
+        "email": MessageLookupByLibrary.simpleMessage("Электронная почта"),
+        "enterConfirmPassword": MessageLookupByLibrary.simpleMessage(
+            "Введите подтверждение пароля"),
+        "enterEmail": MessageLookupByLibrary.simpleMessage(
+            "Введите адрес электронной почты"),
+        "enterFirstName": MessageLookupByLibrary.simpleMessage("Введите имя"),
+        "enterLastName":
+            MessageLookupByLibrary.simpleMessage("Введите фамилию"),
+        "enterPassword": MessageLookupByLibrary.simpleMessage("Введите пароль"),
+        "firstName": MessageLookupByLibrary.simpleMessage("Имя"),
+        "forgotPassword":
+            MessageLookupByLibrary.simpleMessage("Забыли пароль?"),
+        "home": MessageLookupByLibrary.simpleMessage("Дом"),
+        "lastName": MessageLookupByLibrary.simpleMessage("Фамилия"),
+        "magic_chat": MessageLookupByLibrary.simpleMessage("Волшебный чат"),
+        "orSignUpWith": MessageLookupByLibrary.simpleMessage(
+            "Или зарегистрироваться с помощью"),
+        "password": MessageLookupByLibrary.simpleMessage("Пароль"),
+        "profile": MessageLookupByLibrary.simpleMessage("профиль"),
+        "signIn": MessageLookupByLibrary.simpleMessage("Войти"),
+        "signUp": MessageLookupByLibrary.simpleMessage("Зарегистрироваться"),
+        "welcome": MessageLookupByLibrary.simpleMessage("Добро пожаловать"),
+        "welcomeBack": MessageLookupByLibrary.simpleMessage("С возвращением"),
+        "welcomeBackSubtitle": MessageLookupByLibrary.simpleMessage(
+            "Все славные луны любят друг друга, только у великих солнц есть благодать"),
+        "welcomeSubtitle": MessageLookupByLibrary.simpleMessage(
+            "Все великолепные луны любят друг друга, только великие солнца обладают благодатью")
+      };
 }
