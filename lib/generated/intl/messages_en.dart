cf40d76d167fc1ec4aaf78acde0d825f8ceb4f45
Azimdjan <azimdjan18@gmail.com>
2022-10-05 10:00:50 +0500

    advice feature almost done

--- lib/generated/intl/messages_en.dart
+++ lib/generated/intl/messages_en.dart
@@ -21,35 +21,5 @@ class MessageLookup extends MessageLookupByLibrary {
   String get localeName => 'en';
 
   final messages = _notInlinedMessages(_notInlinedMessages);
-  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
-        "advices": MessageLookupByLibrary.simpleMessage("Advices"),
-        "calendar": MessageLookupByLibrary.simpleMessage("Calendar"),
-        "confirmPassword":
-            MessageLookupByLibrary.simpleMessage("Confirm Password"),
-        "email": MessageLookupByLibrary.simpleMessage("Email"),
-        "enterConfirmPassword":
-            MessageLookupByLibrary.simpleMessage("Enter confirm password"),
-        "enterEmail": MessageLookupByLibrary.simpleMessage("Enter email"),
-        "enterFirstName":
-            MessageLookupByLibrary.simpleMessage("Enter firstname"),
-        "enterLastName": MessageLookupByLibrary.simpleMessage("Enter lastname"),
-        "enterPassword": MessageLookupByLibrary.simpleMessage("Enter password"),
-        "firstName": MessageLookupByLibrary.simpleMessage("FirstName"),
-        "forgotPassword":
-            MessageLookupByLibrary.simpleMessage("Forgot Password?"),
-        "home": MessageLookupByLibrary.simpleMessage("Home"),
-        "lastName": MessageLookupByLibrary.simpleMessage("LastName"),
-        "magic_chat": MessageLookupByLibrary.simpleMessage("Magic Chat"),
-        "orSignUpWith": MessageLookupByLibrary.simpleMessage("Or Sign Up with"),
-        "password": MessageLookupByLibrary.simpleMessage("Password"),
-        "profile": MessageLookupByLibrary.simpleMessage("profile"),
-        "signIn": MessageLookupByLibrary.simpleMessage("Sign In"),
-        "signUp": MessageLookupByLibrary.simpleMessage("Sign Up"),
-        "welcome": MessageLookupByLibrary.simpleMessage("Welcome"),
-        "welcomeBack": MessageLookupByLibrary.simpleMessage("Welcome Back"),
-        "welcomeBackSubtitle": MessageLookupByLibrary.simpleMessage(
-            "All honorable moons love each other, only great suns have grace"),
-        "welcomeSubtitle": MessageLookupByLibrary.simpleMessage(
-            "All honorable moons love each other, only great suns have grace")
-      };
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{};
 }
